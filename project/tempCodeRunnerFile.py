25d91f44b16e859574f20128c69a586e0efe6030
CHELLAH Nawel <chellah.nawel@gmail.com>
2025-03-25 23:25:55 +0100

    Delete project/tempCodeRunnerFile.py

--- project/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-# 1️⃣ Charger les données depuis un fichier texte (exemple : "data.txt")
-
-def lire_donnees(fichier):
-    with open(fichier, "r") as f:
-        valeurs = [float(ligne.strip()) 
-        for ligne in f.readlines()]
-    return valeurs
