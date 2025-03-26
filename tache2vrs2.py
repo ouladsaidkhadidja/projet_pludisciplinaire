3fe8e720c7b656a73106a75bcc8f3411677b6b0d
cami-06 <155109780+cami-06@users.noreply.github.com>
2025-03-26 12:21:22 +0100

    Update tache2vrs2.py

--- tache2vrs2.py
+++ tache2vrs2.py
@@ -1,7 +1,8 @@
 import numpy as np 
+
 import matplotlib.pyplot as plt
-import traceback
 from matplotlib.widgets import Button
+import traceback
 
 def readCoord(filename, step):
     radius = []
@@ -18,16 +19,24 @@ def readCoord(filename, step):
         if maxv != 0:
             radius = [r + abs(maxv) for r in radius]
     
-    indices = np.arange(0, len(radius), step)  # Sélectionner les indices avec l'incrémentation choisie
-    radius = [radius[i] for i in indices]  # Filtrer les valeurs de radius
+    indices = np.arange(0, len(radius), step)  # Sélectionner avc l'incrémentation choisie
+    radius = [radius[i] for i in indices]  # Filtrer les valeurs 
     theta = np.radians(indices + 1)  # Ajuster theta pour correspondre aux indices
     
     return radius, theta
 
-nomFichier = "C:\\Users\\Thinkpad T490\\Downloads\\CRMG1SG12019 (1).atn"
+
 step_choice = 1  # Valeur par défaut
 
-def update_plot(step):
+#buttom
+
+def onb1(event):
+    updateplot(1)
+
+def onb5(event):
+    updateplot(5)
+nomFichier = "C:\\Users\\Thinkpad T490\\Downloads\\CRMG1SG12019 (1).atn"
+def updateplot(step):
     global step_choice
     step_choice = step
     radius, theta = readCoord(nomFichier, step_choice)
@@ -38,23 +47,17 @@ def update_plot(step):
     plt.title("Polar plot")
     plt.draw()
 
-def on_button_1(event):
-    update_plot(1)
-
-def on_button_5(event):
-    update_plot(5)
-
 fig, ax = plt.subplots(figsize=(6, 6), subplot_kw={'projection': 'polar'})
 plt.subplots_adjust(bottom=0.2)
-update_plot(step_choice)
+updateplot(step_choice)
 
-ax_button_1 = plt.axes([0.3, 0.05, 0.1, 0.075])
-ax_button_5 = plt.axes([0.6, 0.05, 0.1, 0.075])
+ax1 = plt.axes([0.3, 0.05, 0.1, 0.075])
+ax5 = plt.axes([0.6, 0.05, 0.1, 0.075])
 
-button_1 = Button(ax_button_1, '1°')
-button_5 = Button(ax_button_5, '5°')
+but1 = Button(ax1, '1°')
+but5 = Button(ax5, '5°')
 
-button_1.on_clicked(on_button_1)
-button_5.on_clicked(on_button_5)
+but1.on_clicked(onb1)
+but5.on_clicked(onb5)
 
 plt.show()
